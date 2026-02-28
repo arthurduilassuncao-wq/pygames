fd8b676469b4ed8326f51b7430af5609e52924c6
arthurduilassuncao-wq <arthurduilassuncao@gmail.com>
2026-02-28 00:46:10 -0300

    criei a tela de vitoria e coloquei a imagem do heroi na tela de parabens

--- meu_jogo2.py
+++ meu_jogo2.py
@@ -1,81 +1,51 @@
 import pygame
+
 pygame.init()
-
-
 
 # 2. Criar a tela do jogo
 
 tela = pygame.display.set_mode((800, 600))
-
 pygame.display.set_caption("A Ascensão de Shadow Monarchy")
 
 jogador_pos = [400, 300]
 
-
-
 # 3. O Loop do Jogo
-
 velocidade = 5
-
 rodando = True
-
 relogio = pygame.time.Clock()
 
 while rodando:
 
     # Verificador de eventos (teclado, mouse, fechar janela)
-
     for evento in pygame.event.get():
-
         teclas = pygame.key.get_pressed()
 
-
-
     if teclas[pygame.K_a]:
-
         jogador_pos[0] -= velocidade
-
     if teclas[pygame.K_d]:
-
         jogador_pos[0] += velocidade
-
     if teclas[pygame.K_w]:
-
         jogador_pos[1] -= velocidade
-
     if teclas[pygame.K_s]:
 
         jogador_pos[1] += velocidade
 
-
-
     relogio.tick(60)
 
-
-
     if evento.type == pygame.QUIT:
-
             rodando = False
-
-
 
     # Preencher a tela com uma cor (R, G, B)
 
     tela.fill((30, 30, 30))
 
-
-
     # Desenhar o jogador (um quadrado roxo)
 
     pygame.draw.rect(tela, (128, 0, 128), (jogador_pos[0], jogador_pos[1], 50, 50))
 
-
-
     # Atualizar a tela
 
     pygame.display.flip()
-
-
 
 # Encerrar o jogo
 
--- meu_jogo5.py
+++ meu_jogo5.py
@@ -1,11 +1,11 @@
-import pygame
+﻿import pygame
 import random
 
 pygame.init()
 
 fonte = pygame.font.SysFont("Arial", 30, bold=True)
 tela = pygame.display.set_mode((800, 600))
-pygame.display.set_caption("A Ascensão de Shadow Monarchy")
+pygame.display.set_caption("A Ascensao de Shadow Monarchy")
 
 # Dados do Jogador
 jogador_pos = [400, 300]
@@ -32,8 +32,10 @@ relogio = pygame.time.Clock()
 rodando = True
 #cor_botao = (255, 255, 0) # cor do botao do menu
 cor_texto_botao = (255, 255, 255) # cor do texto do botao do menu
-botao_rect = pygame.Rect(300, 250, 200, 50) #posiçao e tamanho do botao
+botao_rect = pygame.Rect(300, 250, 200, 50) #posicao e tamanho do botao
 estado = "MENU"
+parede_rect = pygame.Rect(300, 400, 200, 50)
+
 
 while rodando:
     for evento in pygame.event.get():
@@ -46,7 +48,7 @@ while rodando:
                 if botao_rect.collidepoint(evento.pos):
                     estado = "JOGANDO"
 
-    # --- LÓGICA DO MENU ---
+    # --- LOGICA DO MENU ---
     if estado == "MENU":
         tela.fill((20, 20, 20))
 
@@ -59,7 +61,6 @@ while rodando:
 
         #desenha o botao do menu
         pygame.draw.rect(tela, cor_atual_botao, botao_rect)
-        pygame.draw.rect(tela, cor_atual_botao, botao_rect)
         #texto do botao do menu
         txt_start = fonte.render("START GAME", True, (0, 0, 0))
         tela.blit(txt_start, (325, 260))
@@ -67,40 +68,51 @@ while rodando:
         titulo = fonte.render("A ASCENCAO DE SHADOW MONARCHY" , True, (255, 215, 0))
         tela.blit(titulo, (150, 150))
 
-    # --- LÓGICA DO JOGO ---
+    # --- LÃ“GICA DO JOGO ---
     
     elif estado == "JOGANDO":
-        # 1. Lógica de Velocidade do Jogador
+        # 1. LÃ³gica de Velocidade do Jogador
         velocidade_jogador = velocidade_base + pontos
         if pontos >= 5:
             velocidade_jogador = velocidade_base + 10
 
-        # 2. Movimentação do Jogador
+        pos_antiga_x = jogador_pos[0]
+        pos_antiga_y = jogador_pos[1]
+
+        # 2. MovimentaÃ§Ã£o do Jogador
         teclas = pygame.key.get_pressed()
         if teclas[pygame.K_a]: jogador_pos[0] -= velocidade_jogador
         if teclas[pygame.K_d]: jogador_pos[0] += velocidade_jogador
         if teclas[pygame.K_w]: jogador_pos[1] -= velocidade_jogador
         if teclas[pygame.K_s]: jogador_pos[1] += velocidade_jogador
 
-        # --- NOVO: IA do Inimigo (Perseguição) ---
+        # --- NOVO: IA do Inimigo (PerseguiÃ§Ã£o) ---
         if inimigo_pos[0] < jogador_pos[0]: inimigo_pos[0] += velocidade_inimigo
         if inimigo_pos[0] > jogador_pos[0]: inimigo_pos[0] -= velocidade_inimigo
         if inimigo_pos[1] < jogador_pos[1]: inimigo_pos[1] += velocidade_inimigo
         if inimigo_pos[1] > jogador_pos[1]: inimigo_pos[1] -= velocidade_inimigo
         
-        # 3. Retângulos de Colisão
+        # 3. RetÃ¢ngulos de ColisÃ£o
         jogador_rect = pygame.Rect(jogador_pos[0], jogador_pos[1], 50, 50)
         item_rect = pygame.Rect(item_pos[0], item_pos[1], 20, 20)
         inimigo_rect = pygame.Rect(inimigo_pos[0], inimigo_pos[1], 40, 40) 
 
-        # 4. Colisão com Item
+        if jogador_rect.colliderect(parede_rect):
+    # O herÃ³i volta para a posiÃ§Ã£o que ele tinha na memÃ³ria!
+            jogador_pos[0] = pos_antiga_x
+            jogador_pos[1] = pos_antiga_y
+
+        # 4. ColisÃ£o com Item
         if jogador_rect.colliderect(item_rect):
             pontos += 1
             item_pos[0] = random.randint(50, 750)
             item_pos[1] = random.randint(50, 550)
 
-        # --- NOVO: Colisão com Inimigo (Game Over / Reset) ---
-        # CORREÇÃO: Usar jogador_rect, não jogador_pos
+        if pontos >= 10:
+            estado = "VITORIA"
+
+        # --- NOVO: ColisÃ£o com Inimigo (Game Over / Reset) ---
+        # CORREÃ‡ÃƒO: Usar jogador_rect, nÃ£o jogador_pos
         if jogador_rect.colliderect(inimigo_rect):
             print("O Vulto te pegou!")
             pontos = 0 
@@ -109,6 +121,8 @@ while rodando:
 
         # 5. Desenho
         tela.fill((30, 30, 30))
+        pygame.draw.rect(tela, (100, 100, 100), parede_rect)  
+    
         
         # Texto do Placar
         cor_texto = (255, 255, 255)
@@ -124,6 +138,22 @@ while rodando:
         tela.blit(img_heroi, (jogador_pos[0], jogador_pos[1]))
         tela.blit(img_monstro, (inimigo_pos[0], inimigo_pos[1]))
         tela.blit(img_item, (item_pos[0], item_pos[1]))
+
+    elif estado == "VITORIA":
+        tela.fill((0, 0, 0))
+        tela.blit(img_heroi, (375, 150,))    
+        msg = fonte.render("VOCE SE TORNOU O REI DAS SOMBRAS!", True, (255, 215, 0))
+        msg2 = fonte.render("Aperte R para reniciar o jogo", True, (255, 255, 255))
+
+        tela.blit(msg, (150, 250))
+        tela.blit(msg2,(220, 320))
+
+        teclas = pygame.key.get_pressed()
+        if teclas[pygame.K_r]:
+            pontos = 0
+            jogador_pos = [400, 300]
+            inimigo_pos = [0, 0]
+            estado = "JOGANDO"
 
     pygame.display.flip()
     relogio.tick(60)
